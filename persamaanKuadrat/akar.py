6af54a5e86683753942e634737594dd05c2b3aa6
ikraam08 <aibunibun@gmail.com>
2022-12-07 01:17:21 -0800

    tambah file py

---
+++ persamaanKuadrat/akar.py
@@ -0,0 +1,31 @@
+import cmath as cm
+
+print("Bentuk Persamaan Kuadrat : ax^2+bx+c= 0")
+
+# Pengimputan
+a = float(input ("a = "))
+b = float(input ("b = "))
+c = float(input ("c = "))
+
+# Mengecek nilai a = ?
+if a==0 :
+    print("Nilai Tidak boleh 0!")
+else :
+    D = b** 2 -4*a*c
+    # Mengecek Nilai Diskriminan
+    if D>0:
+        x1 = (-b+cm.sqrt(D))/(2*a)
+        x2 = (-b-cm.sqrt(D))/(2*a)
+        print("jenis Akarnya 2 Akarnya Berbeda Dan Real")
+    elif D==0 :
+        x1 =(-b/(2*a))
+        x2 =(-b/(2*a))
+        print("Jenis Akarnya Adalah 2 Yang Sama Dan Sama Dan Real")
+    else :
+        x1 = (-b+cm.sqrt(D))/(2*a)
+        x2 = (-b+cm.sqrt(D))/(2*a)
+        print("jenis Akarnya 2 Akarnya Berbeda Dan Imajiner")
+print("akar dari Persamaan kuadrat adalah" +str(x1)+" dan "+str(x2))
+print("Deskriminan\t:",D)
+
+
